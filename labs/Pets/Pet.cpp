cb24da4dce4794329a980d6adce92cba078d7d42
Zachary Rust <zrust001@student.butte.edu>
2018-02-19 00:52:32 +0000

    Pets

---
+++ labs/Pets/Pet.cpp
@@ -0,0 +1,57 @@
+#include "pet.h"
+
+Pet::Pet()
+{
+    name_ = "blank";
+    species_ = 0;
+    nueteredSpayed_ = false;
+    talks_ = false;
+}
+
+Pet::Pet(string name, int species, bool neutered_spayed, bool talks)
+{
+    name_ = name;
+    species_ = species;
+    neutered_spayed_ = neutered_spayed_;
+    talks_ = talks;
+}
+
+void Pet::SetName(string name)
+{
+    name_ = name;
+}
+
+string Pet::GetName()
+{
+    return name_;
+}
+
+void Pet::SetSpecies(int species)
+{
+    species_ = species;
+}
+
+int Pet::GetSpecies()
+{
+    return species_;
+}
+
+void Pet::SetNeuteredSpayed(bool neutered_spayed)
+{
+    neutered_spayed_ = neutered_spayed;
+}
+
+bool Pet::GetNeuteredSpayed()
+{
+    return neutered_spayed_;
+}
+
+void Pet::SetTalks(bool talks)
+{
+    talks_ = talks;
+}
+
+bool Pet::GetTalks()
+{
+    return talks_;
+}
---
+++ labs/Pets/pet.h
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+using std::ostream
+using std::string
+
+class Pet
+{
+
+    public:
+    //default constructor for pet that defaults all values to 0, false or blank.
+    Pet();
+    
+    //write appropriate overloaded constructors
+    Pet(string name, int species, bool neutered_spayed, bool talks);
+    
+    //write get and set functions for each variable
+    void SetName(string name);
+    string GetName();
+    
+    void SetSpecies(int species);
+    int GetSpecies();
+    
+    void SetNeuteredSpayed(bool neutered_spayed);
+    bool GetNeuteredSpayed();
+    
+    void SetTalks(bool talks);
+    bool GetTalks();
+    
+    void print();
+
+    protected:
+
+    string name_;
+
+    int species_;
+
+    bool nueteredSpayed_;
+
+    bool talks_;
+
+};
